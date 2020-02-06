a0da47c911c90e927426f0a953fd136b8acdbb54
sandy-ji-code <sandeepypb@gmail.com>
2020-02-06 11:53:01 +0530

    delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-
-class Company(models.Model):
-    date = models.DateField()
-    time = models.TimeField()
-
-
-c = Company(date=datetime.datetime.now(), time=datetime.datetime.now())
